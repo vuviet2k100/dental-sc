2394e5751b52d7ca41fa80779c223475fdf529af
vuviet2k100 <vuviet2k100@gmail.com>
2026-06-21 02:16:01 +0700

    final

---
+++ frontend/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Chào mừng đến với Hệ thống Quản lý Nha khoa</h1>
+      <a href="/login">Đi tới trang Đăng nhập</a>
+    </div>
+  );
+}
